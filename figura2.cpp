725eb8ed8a65dd4a147c2a767172ddfa688425f6
Eduardo Davila <eduardosebastiandavilazevallos.guadalupe@alumnado.fundacionloyola.net>
2026-02-26 23:17:28 +0100

    Agregar

--- figura2.cpp
+++ figura2.cpp
@@ -7,7 +7,7 @@ main(){
 	cout << "Introduce un numero: "; cin >> n;
 	cout << endl;
 	for (int i = 0; i <= n-1; i++){
-		for (int j = 0; j <=n-1 ; j++){
+		for (int j = 0; j <=n-1; j++){
 			if (j == (n-1)/2 || i == (n-1)/2 || i == 0 && j < (n+1)/2 || i == n-1 && j > ((n-1)/2)-1 || j == 0 && i > ((n-1)/2)-1 || j == n-1 && i < (n+1)/2) {
 				cout << "*";	
 			} else {
