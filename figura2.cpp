4d755f771694e97e308e59a6c1c2b353d326b6a9
Eduardo Davila <eduardosebastiandavilazevallos.guadalupe@alumnado.fundacionloyola.net>
2026-02-26 23:16:21 +0100

    Update figura2.cpp
    Refactor nested loops for clarity and consistency.

--- figura2.cpp
+++ figura2.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 //Autor: Eduardo Davila
-//Simbolo del pintor austriaco
 //Hecho únicamente con fines educativos y no representa ninguna inclinación política.
 main(){
 	int n;
 	cout << "Introduce un numero: "; cin >> n;
 	cout << endl;
-	for (int i=0; i<=n-1; i++){
-		for (int j=0; j<=n-1; j++){
-			if (j == (n-1)/2 || i == (n-1)/2 || i==0 && j < (n+1)/2 || i==n-1 && j>((n-1)/2)-1 || j==0 && i>((n-1)/2)-1 || j==n-1 && i<(n+1)/2){
+	for (int i = 0; i <= n-1; i++){
+		for (int j = 0; j <=n-1 ; j++){
+			if (j == (n-1)/2 || i == (n-1)/2 || i == 0 && j < (n+1)/2 || i == n-1 && j > ((n-1)/2)-1 || j == 0 && i > ((n-1)/2)-1 || j == n-1 && i < (n+1)/2) {
 				cout << "*";	
 			} else {
 				cout << " ";
